7292e021abd3c5d8b758dab3bb8cce7d3d15573a
BalancedCode <kevin@balancedcode.com>
2022-02-04 15:54:36 +0100

    added list_files_in_prefix and unittests

--- xetra/common/s3.py
+++ xetra/common/s3.py
@@ -23,8 +23,14 @@ class S3BucketConnector():
         self._s3 = self.session.resource(service_name='s3', endpoint_url=endpoint_url)
         self._bucket = self._s3.Bucket(bucket)
 
-    def list_files_in_prefix(self):
-        pass
+    def list_files_in_prefix(self, prefix: str):
+        """
+        Listing all files with a prefix on the S3 bucket
+        :param prefix: filename prefix on the S3 bucket used to filter the files
+        :return: List of all matching file names containing the prefix in the key
+        """
+        files = [obj.key for obj in self._bucket.objects.filter(Prefix=prefix)]
+        return files
 
     def read_csv_to_df(self):
         pass
